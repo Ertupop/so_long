3bd1b24c4a41a48dae8e3ecbc4b62d8f58c6b544
Ertupop <rstrub@student.42.fr>
2022-05-09 16:05:06 +0200

    image refaite

--- SRC/ft_load_img.c
+++ SRC/ft_load_img.c
@@ -6,7 +6,7 @@
 /*   By: ertupop <ertupop@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 15:09:49 by ertupop           #+#    #+#             */
-/*   Updated: 2022/04/27 15:39:00 by ertupop          ###   ########.fr       */
+/*   Updated: 2022/05/09 15:05:20 by ertupop          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 void	*ft_load_img(t_vars *so_long)
 {
 	int	x;
-	int	y;
 
-	so_long->collect = mlx_xpm_file_to_image(so_long->mlx, "../img/collect.xpm", &x, &y);
-	so_long->exit = "../img/exit.xpm";
-	so_long->ground = "../img/ground.xpm";
-	so_long->wall = "../img/wall.xpm";
+	x = 32;
+	so_long->collect = mlx_xpm_file_to_image(so_long->mlx, "../img/collect.xpm", &x, &x);
+	so_long->exit = mlx_xpm_file_to_image(so_long->mlx, "../img/exit.xpm", &x, &x);
+	so_long->ground = mlx_xpm_file_to_image(so_long->mlx, "../img/ground.xpm", &x, &x);
+	so_long->wall = mlx_xpm_file_to_image(so_long->mlx, "../img/wall.xpm", &x, &x);
 }
